e1ab0494c40528388d6d98b3569019c4b80e09d9
Joel Kwong <joelckwong@yahoo.com>
2025-07-29 10:20:59 -0400

    fixed typo

--- README.md
+++ README.md
@@ -2,7 +2,7 @@ Build a STIG hardened RHEL8 AMI
 ```
 cd ~/repos
 git clone https://github.com/joelckwong/packer-aws.git
-cd packer-aws/aws/
+cd packer-aws/
 packer build -machine-readable packer-rhel8-stig.json | tee ~/packer-rhel8-stig.out
 aws ec2 describe-images --owners self
 ```
--- provisioners/scripts/oscap-stig9.sh
+++ provisioners/scripts/oscap-stig9.sh
@@ -5,7 +5,7 @@ sudo yum install -y openscap-scanner scap-security-guide audispd-plugins libcap-
 sudo sed -i '/nopasswd.*true/d' /usr/share/xml/scap/ssg/content/ssg-rhel9-ds.xml
 sudo oscap xccdf eval --remediate --profile xccdf_org.ssgproject.content_profile_stig --results ~/oscap-results.xml --report ~/oscap-report.html --fetch-remote-resources /usr/share/xml/scap/ssg/content/ssg-rhel9-ds.xml
 sudo oscap xccdf eval --profile xccdf_org.ssgproject.content_profile_stig --results ~/oscap-results.xml --report ~/oscap-report.html --fetch-remote-resources /usr/share/xml/scap/ssg/content/ssg-rhel9-ds.xml
-curl -O https://public.cyber.mil/wp-content/uploads/stigs/zip/scc-5.10.2_rhel9_oracle-linux9_x86_64_bundle.zip
+curl -O https://dl.dod.cyber.mil/wp-content/uploads/stigs/zip/scc-5.10.2_rhel9_oracle-linux9_x86_64_bundle.zip
 unzip scc-5.10.2_rhel9_oracle-linux8_x86_64_bundle.zip
 sudo rpm -ivh scc-5.10.2_rhel9_x86_64/scc-5.10.2.rhel9.x86_64.rpm
 sudo /opt/scc/cscc
