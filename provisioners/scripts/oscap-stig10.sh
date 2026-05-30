cc04049c713cc1bfa16d22b7a4ce6a8960549dc9
Joel Kwong <31670777+joelckwong@users.noreply.github.com>
2026-05-30 19:04:50 -0400

    Update oscap-stig10.sh for RHEL 10 compatibility

---
+++ provisioners/scripts/oscap-stig10.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -ex
+# Run openscap stig scan
+sudo yum install -y openscap-scanner scap-security-guide audispd-plugins libcap-ng-utils unzip
+#curl -O https://dl.dod.cyber.mil/wp-content/uploads/stigs/zip/scc-5.14.1_rhel10_oracle-linux10_x86_64_bundle.zip
+#unzip scc-5.14.1_rhel10_oracle-linux10_x86_64_bundle.zip
+#sudo rpm -ivh scc-5.14.1_rhel10_x86_64/scc-5.14.1.rhel10.x86_64.rpm
+sudo sed -i '/xccdf_org\.ssgproject\.content_rule_sudo_remove_nopasswd/s/selected="true"/selected="false"/' /usr/share/xml/scap/ssg/content/ssg-rhel10-ds.xml
+sudo oscap xccdf eval --remediate --profile xccdf_org.ssgproject.content_profile_stig --results ~/rhel10-oscap-results.xml --report ~/rhel10-oscap-report.html --fetch-remote-resources /usr/share/xml/scap/ssg/content/ssg-rhel10-ds.xml
+sudo oscap xccdf eval --profile xccdf_org.ssgproject.content_profile_stig --results ~/rhel10-oscap-results.xml --report ~/rhel10-oscap-report.html --fetch-remote-resources /usr/share/xml/scap/ssg/content/ssg-rhel10-ds.xml
+#sudo /opt/scc/cscc
